09bea30ee6713f64b4f1baecf7d4d7152424c530
test <mauro.henrique613@gmail.com>
2023-12-27 16:54:36 -0300

    - Added react router dom
    - Created a wrapper for the sidebar and outlet nesting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import './App.css'
-import { HamburguerBtn } from './components/HamburgerBtn'
-import { Sidebar } from './components/Sidebar'
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { SidebarWrapper } from './pages/SidebarWrapper'
 
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
 
-  const switchSidebar = () => {
-    setIsSidebarOpen(prev => !prev)
-  }
 
   return (
-    <div className='relative w-screen h-screen'>
-      <Sidebar isExpanded={isSidebarOpen} />
-      <div
-        className={`absolute w-full h-full transition-colors ${isSidebarOpen ? "bg-black/5" : ""} z-10`}
-        onClick={isSidebarOpen ? switchSidebar : () => { }}
-      />
-      <div className="absolute top-6 right-6 z-20">
-        <HamburguerBtn isOpen={isSidebarOpen} onClick={switchSidebar} />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SidebarWrapper />}>
+          <Route index element={<>oi</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/SidebarWrapper/index.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import { HamburguerBtn } from "../../components/HamburgerBtn"
+import { Sidebar } from "../../components/Sidebar"
+import { Outlet } from "react-router-dom"
+
+export const SidebarWrapper = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+
+    const switchSidebar = () => {
+        setIsSidebarOpen(prev => !prev)
+    }
+
+    return (
+        <div className='relative w-screen h-screen'>
+            <Sidebar isExpanded={isSidebarOpen} />
+            <div
+                className={`absolute w-full h-full transition-colors ${isSidebarOpen ? "bg-black/5" : ""} z-10`}
+                onClick={isSidebarOpen ? switchSidebar : () => { }}
+            />
+            <div className="absolute top-6 right-6 z-20">
+                <HamburguerBtn isOpen={isSidebarOpen} onClick={switchSidebar} />
+            </div>
+            <Outlet />
+        </div>
+    )
+}
